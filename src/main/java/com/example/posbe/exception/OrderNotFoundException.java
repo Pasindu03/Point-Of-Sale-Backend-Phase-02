5d532b356338afb82192a114ebb47d350abb892e
Pasindu03 <pasindu.yapa03@gmail.com>
2024-10-07 02:56:33 +0530

    Finished Exceptions

---
+++ src/main/java/com/example/posbe/exception/OrderNotFoundException.java
@@ -0,0 +1,15 @@
+package com.example.posbe.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public OrderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OrderNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
