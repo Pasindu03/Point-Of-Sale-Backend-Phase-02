7cf047a5eae0593f912cd4fb571c93d137b2b7d8
Pasindu03 <pasindu.yapa03@gmail.com>
2024-10-07 02:49:16 +0530

    Finished AppUtil

--- src/main/java/com/example/posbe/util/AppUtil.java
+++ src/main/java/com/example/posbe/util/AppUtil.java
@@ -3,7 +3,21 @@ package com.example.posbe.util;
 import java.util.UUID;
 
 public class AppUtil {
+    public static String generateUserId(){
+        return "USERID-" + UUID.randomUUID();
+    }
+
     public static String generateCustomerId(){
         return "CUSID-" + UUID.randomUUID();
     }
+
+    public static String generateItemId(){
+        return "ITEMID-" + UUID.randomUUID();
+    }
+
+    public static String generateOrderId(){
+        return "ORDERID-" + UUID.randomUUID();
+    }
+
+    public static String generateOrderDetailId(){ return "ORDERDETAILID-" + UUID.randomUUID();}
 }
